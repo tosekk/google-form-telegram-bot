a467baf304daca8633f78b191e301566c9ce7de0
Zhandos Kadyrkulov <53704236+tosekk@users.noreply.github.com>
2022-03-17 13:44:10 +0600

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-"""This file holds token and google form urls
-"""
-
-TOKEN = "5261868891:AAHbcoGU1Bc2_Ex2hTB3laQOW6y2beIZnNI"
-view_url = "https://docs.google.com/forms/d/e/1FAIpQLSfoLQOADIAeL93CHuXCg9w1fhqL8wTLmc8V3L-qGAWXlx5cLA/viewform"
-response_url = "https://docs.google.com/forms/d/e/1FAIpQLSfoLQOADIAeL93CHuXCg9w1fhqL8wTLmc8V3L-qGAWXlx5cLA/formResponse"
